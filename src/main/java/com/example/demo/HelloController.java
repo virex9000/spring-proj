0b86389b00e287e650f885bd44d73daee5e14734
virex9000 <virex9000@gmail.com>
2025-01-26 14:58:14 +0530

    Initial commit: Created a basic Spring Boot project with a REST API

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+
+        return "Helo";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greet(@PathVariable String name)
+    {
+        return "hello"+name;
+
+    }
+
+}
